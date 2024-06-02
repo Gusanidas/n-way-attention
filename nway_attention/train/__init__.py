deeece24034d1da69b29d891d7b9cd2803f407c9
Sergio Alvarez <sbalanya@amazon.es>
2024-06-02 07:40:36 +0200

    create train module

---
+++ nway_attention/train/__init__.py
@@ -0,0 +1,1 @@
+from ._train_student_with_teacher import train_student_with_teacher
---
+++ nway_attention/train/_train_student_with_teacher.py
@@ -0,0 +1,44 @@
+import time
+import torch
+from torch import nn, optim
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters())
+
+def train_student_with_teacher(student_model, teacher_model, hidden_size, num_epochs=2500, batch_size=256, seq_len=42, learning_rate=0.002):
+    t0 = time.time()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(student_model.parameters(), lr=learning_rate)
+
+    # Initialize cosine scheduler
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+
+    student_model.to(device)
+    teacher_model.to(device)
+
+    print(f"Count parameters student = {count_parameters(student_model)}")
+    print(f"Count parameters teacher = {count_parameters(teacher_model)}")
+    print(f"teacher = {teacher_model}")
+    print(f"student_model = {student_model}")
+
+    for epoch in range(num_epochs):
+        inputs = torch.randn(batch_size, seq_len, hidden_size).to(device)
+        #inputs = torch.randint(0, hidden_size, (batch_size, seq_len))
+
+        with torch.no_grad():
+            teacher_outputs = teacher_model(inputs)
+
+        student_outputs = student_model(inputs)
+
+        loss = criterion(student_outputs, teacher_outputs)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Step the scheduler
+        scheduler.step()
+
+        if (epoch + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.8f}, time = {time.time()-t0}')
